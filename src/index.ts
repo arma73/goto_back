bb2c510f819227f79cd5ce3a387b91e2db9a7ad7
arma73 <arma.biz9805@gmail.com>
2021-02-09 00:10:04 +0400

    create: initial app entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("we");
+import { Options } from "graphql-yoga";
+import chalk from "chalk";
+import app from "./app";
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT = "/playground";
+const GRAPHQL_ENDPOINT = "/graphql";
+
+const appOptions: Options = {
+    "port": PORT,
+    "playground": PLAYGROUND_ENDPOINT,
+    "endpoint": GRAPHQL_ENDPOINT,
+};
+
+const handleAppStat = () =>
+    console.log(chalk.green(`Listening on port ${PORT}`)); // eslint-disable-line no-console
+
+app.start(appOptions, handleAppStat);
