4f434302c979f655be8c3e489cc4f824ae640c23
arma73 <arma.biz9805@gmail.com>
2021-02-09 23:26:56 +0400

    create: Place entity

---
+++ src/db/entities/Place.ts
@@ -0,0 +1,38 @@
+import {
+    Entity,
+    BaseEntity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    BeforeInsert
+} from "typeorm";
+
+@Entity()
+class Place extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ "type": "text" })
+    name: string;
+
+    @Column({ "type": "double precision", "default": 0 })
+    lat: number;
+
+    @Column({ "type": "double precision", "default": 0 })
+    lng: number;
+
+    @Column({ "type": "text" })
+    address: string;
+
+    @Column({ "type": "boolean", "default": false })
+    isFav: boolean;
+
+    @CreateDateColumn()
+    createdAt: string;
+
+    @UpdateDateColumn()
+    updateAt: string;
+}
+
+export default Place;
