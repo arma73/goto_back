08fbad0ab3d93e737112eb7044b25d4624de66b3
arma73 <arma.biz9805@gmail.com>
2021-02-09 23:56:04 +0400

    add: ride status

--- src/types/enums.d.ts
+++ src/types/enums.d.ts
@@ -2,3 +2,11 @@ export enum VerifyTarget {
     PHONE = "PHONE",
     EMAIL = "EMAIL",
 }
+
+export enum RideStatus {
+    ACCEPTED = "ACCEPTED",
+    FINISHED = "FINISHED",
+    CANCELED = "CANCELED",
+    REQUESTING = "REQUESTING",
+    ONROUTE = "ONROUTE",
+}
