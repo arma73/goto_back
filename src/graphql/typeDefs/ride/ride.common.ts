5c384d8155542b033d92f6489534758aff1f471f
arma73 <arma.biz9805@gmail.com>
2021-02-09 23:56:32 +0400

    create: ride schema

---
+++ src/graphql/typeDefs/ride/ride.common.ts
@@ -0,0 +1,23 @@
+import { gql } from "apollo-server-express";
+
+export const ride = gql`
+    extend type Query {
+        Ride(id: ID!): Ride!
+    }
+
+    type Ride {
+        id: Int!
+        status: String!
+        pickUpAddress: String!
+        pickUpLat: Float!
+        pickUpLng: Float!
+        dropOffAddress: String!
+        dropOffLat: Float!
+        dropOffLng: Float!
+        price: Float!
+        distance: String!
+        duration: String!
+        createdAt: String!
+        updateAt: String
+    }
+`;
