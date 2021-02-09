8374de5173d32a39d80397aba04b493bae04cc28
arma73 <arma.biz9805@gmail.com>
2021-02-09 17:35:29 +0400

    create: initial entity user

---
+++ src/entities/User.ts
@@ -0,0 +1,66 @@
+import { IsEmail } from "class-validator";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ "type": "text", "unique": true })
+    @IsEmail()
+    email: string;
+
+    @Column({ "type": "boolean", "default": false })
+    verifiedEmail: boolean;
+
+    @Column({ "type": "text" })
+    firstName: string;
+
+    @Column({ "type": "text" })
+    lastName: string;
+
+    @Column({ "type": "int" })
+    age: number;
+
+    @Column({ "type": "text" })
+    password: string;
+
+    @Column({ "type": "text" })
+    phoneNumber: string;
+
+    @Column({ "type": "boolean", "default": false })
+    verifiedPhoneNumber: boolean;
+
+    @Column({ "type": "text" })
+    profilePhoto: string;
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    @Column({ "type": "boolean", "default": false })
+    idDriving: boolean;
+
+    @Column({ "type": "boolean", "default": false })
+    isRiding: boolean;
+
+    @Column({ "type": "boolean", "default": false })
+    isTaken: boolean;
+
+    @Column({ "type": "double precision", "default": 0 })
+    lastLng: number;
+
+    @Column({ "type": "double precision", "default": 0 })
+    lastLat: number;
+
+    @Column({ "type": "double precision", "default": 0 })
+    lastOrientation: number;
+
+    @CreateDateColumn()
+    createdAt: string;
+
+    @UpdateDateColumn()
+    updatedAt: string;
+}
+
+export default User;
