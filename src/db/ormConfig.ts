d5f3b86b3b8177483d0ffd522ea764035551fed3
arma73 <arma.biz9805@gmail.com>
2021-02-09 15:52:59 +0400

    add: config for typeorm

---
+++ src/db/ormConfig.ts
@@ -0,0 +1,17 @@
+import { ConnectionOptions } from "typeorm";
+
+const { DB_ENDPOINT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+
+const connectionOptions: ConnectionOptions = {
+    "type": "postgres",
+    "synchronize": true,
+    "logging": true,
+    "entities": ["entity/**/*.*"],
+    "port": 5432,
+    "database": DB_NAME,
+    "host": DB_ENDPOINT,
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+};
+
+export default connectionOptions;
