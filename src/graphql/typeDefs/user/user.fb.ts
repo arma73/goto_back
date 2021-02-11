0be5a5fd677317cece162da9fa4d688d6cb35efa
arma73 <arma.biz9805@gmail.com>
2021-02-11 19:03:38 +0400

    update: user connect fb schema

--- src/graphql/typeDefs/user/user.fb.ts
+++ src/graphql/typeDefs/user/user.fb.ts
@@ -2,17 +2,19 @@ import { gql } from "apollo-server-express";
 
 export const userConnectFB = gql`
     type FacebookConnectResponse {
-        ok: Boolean!
+        success: Boolean!
         error: String
         token: String
     }
 
+    input ConnectFBInput {
+        firstName: String!,
+        lastName: String!,
+        email: String,
+        fbId: String!
+    }
+
     extend type Mutation {
-        FacebookConnect(
-            firstName: String!,
-            lastName: String!,
-            email: String,
-            fbId: String!
-        ): FacebookConnectResponse!
+        facebookConnect(input: ConnectFBInput!): FacebookConnectResponse!
     }
 `;
