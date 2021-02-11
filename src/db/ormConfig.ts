0e7466903c6288409b54e4df26da7d9feef223f2
arma73 <arma.biz9805@gmail.com>
2021-02-11 18:53:49 +0400

    fix: recognize entities

--- src/db/ormConfig.ts
+++ src/db/ormConfig.ts
@@ -1,4 +1,5 @@
 import { ConnectionOptions } from "typeorm";
+import { join } from "path";
 
 const { DB_ENDPOINT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
@@ -6,12 +7,15 @@ const connectionOptions: ConnectionOptions = {
     "type": "postgres",
     "synchronize": true,
     "logging": true,
-    "entities": ["./entities/**/*.*"],
+    "entities": [join(__dirname, "entities", "**", "*.ts")],
     "port": 5432,
     "database": DB_NAME,
     "host": DB_ENDPOINT,
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
+    "cli": {
+        "entitiesDir": "./entities"
+    },
 };
 
 export default connectionOptions;
