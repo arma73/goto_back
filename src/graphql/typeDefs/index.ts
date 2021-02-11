263edcdb3d12b9dcd5459158b527138121b1a8bf
arma73 <arma.biz9805@gmail.com>
2021-02-11 19:04:20 +0400

    update: import all types

--- src/graphql/typeDefs/index.ts
+++ src/graphql/typeDefs/index.ts
@@ -1,6 +1,18 @@
 import { DocumentNode } from "graphql";
-import { user } from "./user";
+import { root } from "./root";
+import { user, userConnectFB } from "./user";
+import { chat, message } from "./chat";
+import { place } from "./place";
+import { ride } from "./ride";
+import { verify } from "./verify";
 
 export const typeDefs: DocumentNode[] = [
-    user
+    root,
+    user,
+    chat,
+    message,
+    userConnectFB,
+    place,
+    ride,
+    verify
 ];
