240247950828fe9a72a6b028db13788250ca31d8
arma73 <arma.biz9805@gmail.com>
2021-02-11 17:45:38 +0400

    refactor: listen to the server in the instance

--- src/app/Server.ts
+++ src/app/Server.ts
@@ -1,40 +1,77 @@
+/* eslint-disable no-console */
 import express, { Application } from "express";
+import { Server as HTTPServer } from "http";
 import { ApolloServer } from "apollo-server-express";
 import { Connection } from "typeorm";
-import connectDB from "../db/connect";
 import cors from "cors";
 import helmet from "helmet";
 import logger from "morgan";
+import chalk from "chalk";
+import DataBase from "../db/DataBase";
 import apolloConfig from "../apollo/config";
+import { CloseServerError } from "../errors";
+import { TEnv } from "../types/custom";
 
 class Server {
-    private connectDB: Connection = connectDB;
-    private app: Application = express();
+    private app: Application;
+    private server: HTTPServer;
+    private port: TEnv;
+    private graphqlPath: string;
 
-    constructor() {
-        this.middlewars();
-        this.initializeApolloServer();
+    constructor(port: TEnv = "8000", graphqlPath = "/api") {
+        this.app = express();
+        this.port = port;
+        this.graphqlPath = graphqlPath;
     }
 
-    private initializeApolloServer(): void {
+    public start = async (): Promise<void> => {
+        try {
+            await this.connectWithDB();
+            this.initializeApolloServer();
+            this.middlewars();
+            this.server = this.getApp()
+                .listen(this.port, this.handleStat);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    /**
+     * @throws {CloseServerError} - the server did not start before shutdown
+     */
+    public close = async (): Promise<HTTPServer> => {
+        if (this.server instanceof HTTPServer) {
+            console.log(chalk.green(`\nthe server was closed successfully on port ${this.port}`));
+            return this.server.close();
+        }
+
+        throw new CloseServerError({ "port": this.port });
+    }
+
+    private initializeApolloServer = (): void => {
         const server = new ApolloServer(apolloConfig);
-        server.applyMiddleware({ "app": this.app, "path": "/api" });
+        server.applyMiddleware({ "app": this.app, "path": this.graphqlPath });
     }
 
     private middlewars = (): void => {
         this.app.use(cors());
         this.app.use(logger("dev"));
         this.app.use(helmet({
-            "contentSecurityPolicy": process.env.NODE_ENV === "production" ? undefined : false
+            "contentSecurityPolicy": process.env.NODE_ENV === "production" ? undefined : false,
         }));
     };
 
-    public getApp(): Application {
+    private handleStat = (): void => {
+        console.log(chalk.green(`Listening on port ${this.port}`));
+    }
+
+    private getApp(): Application {
         return this.app;
     }
 
-    public getConnectDB(): Promise<Connection> {
-        return this.connectDB.connect();
+    private async connectWithDB(): Promise<Connection> {
+        const db = new DataBase();
+        return db.connect();
     }
 }
 
