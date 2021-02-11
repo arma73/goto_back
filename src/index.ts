5eb7f7a63b1888e0e9150cc13b66ef4ab33e35cb
arma73 <arma.biz9805@gmail.com>
2021-02-11 17:41:27 +0400

    refactor: change server linstening locations

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import "./security/env";
-import chalk from "chalk";
 import Server from "./app/Server";
 
 const { PORT } = process.env;
-const server = new Server();
+const server = new Server(PORT);
 
-// eslint-disable-next-line no-console
-const handleAppStat = () => console.log(chalk.green(`Listening on port ${PORT}`));
-
-server.getConnectDB()
-    .then(() => {
-        server.getApp().listen(PORT, handleAppStat);
-    })
-    .catch(error => console.error(error)); // eslint-disable-line no-console
+server.start();
