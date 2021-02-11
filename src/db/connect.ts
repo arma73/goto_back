6c62c63b4192b09c96e5cc1f07c1e59d08f3db53
arma73 <arma.biz9805@gmail.com>
2021-02-11 17:46:19 +0400

    remove: database connection

--- src/db/connect.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata";
-import { getConnectionManager, ConnectionManager, Connection } from "typeorm";
-import connectionOptions from "./ormConfig";
-
-const connectionManager: ConnectionManager = getConnectionManager();
-const connection: Connection = connectionManager.create(connectionOptions);
-
-export default connection;
