e908f76e6ff5903cb1cc0597057a06150cee1dc4
arma73 <arma.biz9805@gmail.com>
2021-02-21 05:43:30 +0400

    update: with export resolver completePhoneVerification

--- src/graphql/resolvers/user/mutations/index.ts
+++ src/graphql/resolvers/user/mutations/index.ts
@@ -2,11 +2,13 @@ import { IResolvers } from "apollo-server-express";
 import { facebookConnect } from "./facebookConnect";
 import { emailSignIn } from "./emailSignIn";
 import { phoneVerification } from "./phoneVerification";
+import { completePhoneVerification } from "./completePhoneVerification";
 
 export const userMutation: IResolvers = {
     "Mutation": {
         facebookConnect,
         emailSignIn,
         phoneVerification,
+        completePhoneVerification,
     },
 };
