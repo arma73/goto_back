663413b625f0d99f6b5d2fd36a649e45b5116407
arma73 <arma.biz9805@gmail.com>
2021-02-22 02:14:43 +0400

    update: with export UnableStartError

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -2,3 +2,4 @@ export * from "./ServerNotFoundError";
 export * from "./CloseServerError";
 export * from "./DataBaseConnectionError";
 export * from "./EnvNotAssignedError";
+export * from "./UnableStartError";
