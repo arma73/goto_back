65f13cd7ca59674b9d0d76799b196f9ef132b260
arma73 <arma.biz9805@gmail.com>
2021-02-22 00:59:55 +0400

    update: with export userProfile

--- src/graphql/typeDefs/user/index.ts
+++ src/graphql/typeDefs/user/index.ts
@@ -2,10 +2,12 @@ import { userCommon } from "./user.common";
 import { userConnectFB } from "./user.fb";
 import { userEmail } from "./user.email";
 import { userPhone } from "./user.phone";
+import { userProfile } from "./user.profile";
 
 export default [
     userCommon,
     userConnectFB,
     userEmail,
-    userPhone
+    userPhone,
+    userProfile
 ];
