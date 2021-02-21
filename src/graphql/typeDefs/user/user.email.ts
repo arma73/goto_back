cb3168643029b30167c2b08e41468a2563a90b65
arma73 <arma.biz9805@gmail.com>
2021-02-21 06:27:29 +0400

    add: email sign up schema

--- src/graphql/typeDefs/user/user.email.ts
+++ src/graphql/typeDefs/user/user.email.ts
@@ -7,12 +7,29 @@ export const userEmail = gql`
         token: String
     }
 
+    type EmailSignUpResponse {
+        success: Boolean!
+        error: String
+        token: String
+    }
+
     input EmailSignInInput {
         email: String!
         password: String!
     }
 
+    input EmailSignUpInput {
+        firstName: String!
+        lastName: String!
+        email: String!
+        password: String!
+        profilePhoto: String!
+        age: Int!
+        phoneNumber: String!
+    }
+
     extend type Mutation {
         emailSignIn(input: EmailSignInInput!): EmailSignInResponse!
+        emailSignUp(input: EmailSignUpInput!): EmailSignUpResponse! 
     }
 `;
