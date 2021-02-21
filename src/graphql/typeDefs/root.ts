3bc42a0747bf8567d54d684e89377c4ffa7fa50c
arma73 <arma.biz9805@gmail.com>
2021-02-22 00:52:50 +0400

    update: https://github.com/ardatan/graphql-tools/issues/648

--- src/graphql/typeDefs/root.ts
+++ src/graphql/typeDefs/root.ts
@@ -1,11 +1,15 @@
 import { gql } from "apollo-server-express";
 
+/**
+ * If try to create an empty Query type to extend later on, Apollo will throw a syntax error.
+ * @see https://github.com/ardatan/graphql-tools/issues/648
+ */
 export const root = gql`
     type Query {
-        root: String
+        _blank: String
     }
 
     type Mutation {
-        root: String
+        _blank: String
     }
 `;
