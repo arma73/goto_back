2444e70f69e922610b6bd73f6aaa9235546d97b1
arma73 <arma.biz9805@gmail.com>
2021-02-22 10:13:00 +0400

    add: NoNullFields converter type

--- src/types/custom.ts
+++ src/types/custom.ts
@@ -1,3 +1,9 @@
 export type TEnv = string | undefined;
 
 export type AllowNull<T> = T | null;
+
+export type NoNullFields<T> = {
+    [K in keyof T]: T[K] extends object // eslint-disable-line @typescript-eslint/ban-types
+        ? NoNullFields<T[K]>
+        : NonNullable<T[K]>
+};
