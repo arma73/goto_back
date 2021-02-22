89cf98e8fac9fe864ebac1275b347b48f1e15aba
arma73 <arma.biz9805@gmail.com>
2021-02-22 08:07:48 +0400

    update: confirmation of sending by email

--- src/graphql/resolvers/user/mutations/emailSignUp.ts
+++ src/graphql/resolvers/user/mutations/emailSignUp.ts
@@ -1,12 +1,15 @@
 import { createJWT } from "../../../../helpers/createJWT";
 import User from "../../../../db/entities/User";
+import Verify from "../../../../db/entities/Verify";
+import { VerifyTarget } from "../../../../db/types/enums";
+import { sendVerificationEmail } from "../../../../helpers/sendEmail";
 import { EmailSignUpMutationArgs, EmailSignUpResponse } from "../types";
 
 export const emailSignUp = async (
     _root: undefined,
     { input }: EmailSignUpMutationArgs
 ): Promise<EmailSignUpResponse> => {
-    const { email } = input;
+    const { email, phoneNumber } = input;
 
     try {
         const existingUser = await User.findOne({
@@ -15,13 +18,39 @@ export const emailSignUp = async (
 
         if (existingUser) {
             return {
-                success: false,
-                error: "Your email is already registered, you can use another email or log in with this email.",
-                token: null,
+                "success": false,
+                "error": "Your email is already registered, you can use another email or log in with this email.",
+                "token": null,
+            };
+        }
+
+        const phoneVerification = await Verify.findOne({
+            "payload": phoneNumber,
+            "verified": true,
+        });
+
+        if (!phoneVerification) {
+            return {
+                "success": false,
+                "error": "You haven't verified your phone number",
+                "token": null,
             };
         }
 
         const createdUser = await User.create({ ...input }).save();
+
+        if (createdUser.email) {
+            const emailVerification = await Verify.create({
+                "payload": createdUser.email,
+                "target": VerifyTarget.EMAIL,
+            }).save();
+
+            await sendVerificationEmail(
+                createdUser.fullName,
+                emailVerification.key
+            );
+        }
+
         const token = createJWT(createdUser.id);
 
         return {
