260c2f98646dea67dc47955dd5b82650c890f739
arma73 <arma.biz9805@gmail.com>
2021-02-22 10:51:26 +0400

    update: with export userDriving

--- src/graphql/typeDefs/user/index.ts
+++ src/graphql/typeDefs/user/index.ts
@@ -3,11 +3,13 @@ import { userConnectFB } from "./user.fb";
 import { userEmail } from "./user.email";
 import { userPhone } from "./user.phone";
 import { userProfile } from "./user.profile";
+import { userDriving } from "./user.driving";
 
 export default [
     userCommon,
     userConnectFB,
     userEmail,
     userPhone,
-    userProfile
+    userProfile,
+    userDriving
 ];
