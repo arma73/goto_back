39a1c8aea3ea3d9acbc9f49e2d347f24d5d4a756
arma73 <arma.biz9805@gmail.com>
2021-02-22 10:13:45 +0400

    update: with fields not null

--- src/apollo/types.ts
+++ src/apollo/types.ts
@@ -1,5 +1,8 @@
 import User from "../db/entities/User";
+import { AllowNull, NoNullFields } from "../types/custom";
 
 export interface ApolloContext {
-    "user": User | null;
+    "user": AllowNull<User>;
 }
+
+export type NoAllowNullApolloContext = NoNullFields<ApolloContext>;
