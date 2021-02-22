75cbe6ac1586a525145a1503fb7ae11a06395f38
arma73 <arma.biz9805@gmail.com>
2021-02-23 03:41:01 +0400

    add: user field in the table place

--- src/db/entities/Place.ts
+++ src/db/entities/Place.ts
@@ -4,8 +4,10 @@ import {
     Column,
     PrimaryGeneratedColumn,
     CreateDateColumn,
-    UpdateDateColumn
+    UpdateDateColumn,
+    ManyToOne
 } from "typeorm";
+import User from "./User";
 
 @Entity()
 class Place extends BaseEntity {
@@ -26,6 +28,9 @@ class Place extends BaseEntity {
 
     @Column({ "type": "boolean", "default": false })
     isFav: boolean;
+
+    @ManyToOne(() => User, user => user.places)
+    user: User;
 
     @CreateDateColumn()
     createdAt: string;
