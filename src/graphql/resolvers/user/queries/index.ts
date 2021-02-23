46f0a6a0c13186b4c465659b3d950e9cb98e65e0
arma73 <arma.biz9805@gmail.com>
2021-02-23 13:54:30 +0400

    update: with export getNearbyDrivers

--- src/graphql/resolvers/user/queries/index.ts
+++ src/graphql/resolvers/user/queries/index.ts
@@ -1,7 +1,9 @@
 import { getMyProfile } from "./getMyProfile";
+import { getNearbyDrivers } from "./getNearbyDrivers";
 
 export const userQuery = {
     "Query": {
         getMyProfile,
+        getNearbyDrivers,
     },
 };
